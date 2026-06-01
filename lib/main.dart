eced17b20e4718d7f56d5255dc7ac0c6d7d40605
1ampupa <craftpupathai@gmail.com>
2026-06-02 00:22:53 +0700

    feat: Create a basic window

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:window_manager/window_manager.dart';
 
-void main() {
-  runApp(
-    const MaterialApp(
+void main() async {
+  // Waiting for framework
+  WidgetsFlutterBinding.ensureInitialized();
+  await windowManager.ensureInitialized();
+
+  WindowOptions windowsOptions = const WindowOptions(
+    size: Size(1000, 750),
+    minimumSize: Size(1000, 750),
+    center: true,
+    title: 'Sonority'
+  );
+
+  windowManager.waitUntilReadyToShow(windowsOptions, () async {
+    await windowManager.show();
+    await windowManager.focus();
+  });
+
+  runApp(const SonorityApp());
+}
+
+class SonorityApp extends StatelessWidget {
+  const SonorityApp({super.key});
+
+  // Build an app
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.black,
-        body: Center(
-          child: Text(
-            'Sonority',
-            style: TextStyle(color: Colors.white, fontSize: 24),
-          )
-        ),
+        backgroundColor: Color(0xFF151515)
       )
-    )
-  );
+    );
+  }
 }
